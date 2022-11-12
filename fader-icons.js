7e26fc485d5ab88d2e08b35dd15a57d0180459b1
Dwerg01 <nardum@gmail.com>
2022-11-12 10:01:10 +0200

    fader icons complete

--- fader-icons.js
+++ fader-icons.js
@@ -3,8 +3,8 @@ var height = 0;
 if(valuesGrid.length > 1){
   setTimeout(function(){
     valuesGrid.forEach(function(item, i){
-      if(height < item.offsetHeight){
-        height = item.offsetHeight;
+      if(height < item.offsetParent ){
+        height = item.offsetParent;
       }
     });
     valuesGrid.forEach(function(item, i){
@@ -12,3 +12,5 @@ if(valuesGrid.length > 1){
     });
   }, 100);    
 }
+
+{/* <script src="{{ 'fader-icons.js' | asset_url }}" defer="defer"></script> */}
